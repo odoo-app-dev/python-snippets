72eed34c48263d620bc9caf7a222b0a0f575899c
Arash Homayounfar <93074385+odoo-app-dev@users.noreply.github.com>
2022-12-18 15:20:04 +0330

    Update ping.py

--- ping.py
+++ ping.py
@@ -7,7 +7,6 @@ def ping(machine_ip):
     if platform.system() == 'Linux':
         response = os.system("ping -c 1 " + machine_ip)
         if response == 0:
-          logging.warning("Biometric Device is Up/Reachable.")
             logging.warning("Biometric Device is Up/Reachable.")
         else:
             logging.warning("Biometric Device is Down/Unreachable.")
