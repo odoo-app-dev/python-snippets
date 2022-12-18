b9e4229836c01b035137b3f809d2d56b0a46c769
Arash Homayounfar <93074385+odoo-app-dev@users.noreply.github.com>
2022-12-18 15:15:30 +0330

    Update ping.py

--- ping.py
+++ ping.py
@@ -3,16 +3,17 @@ import platform
 import subprocess
 import logging
 
-if platform.system() == 'Linux':
-    response = os.system("ping -c 1 " + machine_ip)
-    if response == 0:
-      logging.warning("Biometric Device is Up/Reachable.")
-        logging.warning("Biometric Device is Up/Reachable.")
+def ping(machine_ip):
+    if platform.system() == 'Linux':
+        response = os.system("ping -c 1 " + machine_ip)
+        if response == 0:
+          logging.warning("Biometric Device is Up/Reachable.")
+            logging.warning("Biometric Device is Up/Reachable.")
+        else:
+            logging.warning("Biometric Device is Down/Unreachable.")
     else:
-        logging.warning("Biometric Device is Down/Unreachable.")
-else:
-    prog = subprocess.run(["ping", machine_ip], stdout=subprocess.PIPE)
-    if 'unreachable' in str(prog):
-        logging.warning("Biometric Device is Down/Unreachable.")
-    else:
-        logging.warning("Biometric Device is Up/Reachable.")
+        prog = subprocess.run(["ping", machine_ip], stdout=subprocess.PIPE)
+        if 'unreachable' in str(prog):
+            logging.warning("Biometric Device is Down/Unreachable.")
+        else:
+            logging.warning("Biometric Device is Up/Reachable.")
